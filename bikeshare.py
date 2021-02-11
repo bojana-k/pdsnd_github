1c6cd59fc83569df5afd4e06f3f8aee2b751193f
Bojana Kuzeljevic <bojana.kuzeljevic@gmail.com>
2021-02-11 16:59:19 +0900

    docs: Add comment line to the code

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#Read in data
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
